b2d907dfb6e407aa967d39db27efc8a40861f1fd
mjbatalha <mjcsbatalha@gmail.com>
2024-10-15 16:04:19 +0200

    add basic experiment tracking

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 import numpy as np
 import torch
 import random
+import yaml
 
+from pathlib import Path
 from torch.nn import MSELoss
 from torch.optim import Adam
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
 from tqdm.auto import tqdm
 
 from autoencoder import KLAutoencoder
@@ -46,26 +50,34 @@ class TrainTxt2Img(object):
         self.dl = dataset.get_dataloader()
         self.loss = [LOSSES[k](**v) for k, v in self.tr_cfg["loss"].items()][0]
         self.opt = [OPTIMIZERS[k](self.model.parameters(), **v) for k, v in self.tr_cfg["optimizer"].items()][0]
+        
+        self.writer = SummaryWriter(**self.tr_cfg["writter"])
+        self.save_config(config)
+        self.n_batch = None
 
     def train(self):
-        
-        metrics = {
-            "batch_loss": [],
-            "epoch_loss": [],
-            }
-        
+
+        self.n_batch = 0
         for epoch in tqdm(range(self.tr_cfg["n_epochs"]), desc="epochs"):
-            
-            batch_metrics = self.train_epoch()
-            metrics["batch_loss"].extend(batch_metrics["loss"])
-            metrics["epoch_loss"].append(batch_metrics["loss"][-1])
+
+            imgs = make_grid(self.sample())
+            self.writer.add_image(" || ".join(self.tr_cfg["sample_prompts"]), imgs, epoch)
+
+            metrics = self.train_epoch()
+            self.writer.add_scalar("epoch_loss", np.mean(metrics["batch_loss"]), epoch)
+            self.writer.add_scalar("epoch_log_loss", np.mean(metrics["batch_log_loss"]), epoch)
+
+        imgs = make_grid(self.sample())
+        self.writer.add_image(" || ".join(self.tr_cfg["sample_prompts"]), imgs, epoch + 1)
+
+        self.writer.close()
 
     def train_epoch(self):
 
         self.model.train()
 
-        batch_metrics = {"loss": []}
-        for batch in tqdm(self.dl, desc="batches"):
+        metrics = {"batch_loss": [], "batch_log_loss": []}
+        for batch in tqdm(self.dl, desc="batches", leave=False):
             
             imgs = batch["image"].to(self.device)
             prompts = batch["prompt"]
@@ -81,26 +93,42 @@ class TrainTxt2Img(object):
             loss.backward()
             self.opt.step()
 
-            batch_metrics["loss"].append(loss.item())
+            metrics["batch_loss"].append(loss.item())
+            metrics["batch_log_loss"].append(np.log(loss.item()))
 
-        return batch_metrics
+            self.writer.add_scalar("batch_loss", loss.item(), self.n_batch)
+            self.writer.add_scalar("batch_log_loss", np.log(loss.item()), self.n_batch)
+            self.n_batch += 1
+
+        return metrics
 
     @torch.inference_mode
-    def eval_sample(self):
+    def sample(self):
 
         self.model.eval()
 
         z_shape = self.tr_cfg["z_shape"]
-        prompts = self.tr_cfg["eval_prompts"]
+        prompts = self.tr_cfg["sample_prompts"]
         z = torch.randn((len(prompts), *z_shape)).to(self.device)
         c = self.model.text_conditioning(prompts).to(self.device)
-        
-        for t in reversed(tqdm(range(self.model.n_steps), desc="sampling")):
+        for t in reversed(tqdm(range(self.model.n_steps), desc="sampling", leave=False)):
             t = torch.full((len(prompts),), t).long().to(self.device)
             z = self.model.sample(z, t, c)
-
-
         imgs = self.model.decode(z)
 
         return imgs
+    
+    def save_config(self, config: dict):
+        cfg_path = Path(self.tr_cfg["writter"]["log_dir"]) / "config.yaml"
+        with open(cfg_path, "w") as f:
+            yaml.dump(config, f)
+
+
+if __name__ == "__main__":
+
+    with open("train_config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    trainer = TrainTxt2Img(config)
+    trainer.train()
 
